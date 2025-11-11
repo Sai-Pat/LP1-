61de6cf5e6b9e54474af94b1f8c971b5eff54ad1
Sai Pat <157099340+Sai-Pat@users.noreply.github.com>
2025-11-11 17:58:00 +0530

    Update 1.java

--- 1.java
+++ 1.java
@@ -1,48 +1,44 @@
 import java.util.*;
 
-public class PassOneAssembler {
+public class TinyPassOneAssembler {
     public static void main(String[] args) {
-        // Sample assembly program (can also be taken from file)
-        String program[] = {
-            "START 100",
-            "MOVER AREG, NUM",
-            "ADD BREG, ONE",
-            "MOVEM AREG, RESULT",
-            "ONE DC 1",
-            "NUM DS 1",
-            "RESULT DS 1",
-            "END"
-        };
+        Scanner sc = new Scanner(System.in);
 
-        // Opcode table (MOT)
+        System.out.println("Enter number of lines in program:");
+        int n = sc.nextInt();
+        sc.nextLine(); // consume newline
+
+        String[] program = new String[n];
+        System.out.println("Enter program lines:");
+        for (int i = 0; i < n; i++) {
+            program[i] = sc.nextLine();
+        }
+
+        // Very simple opcode table
         Map<String, String> MOT = new HashMap<>();
         MOT.put("START", "AD");
         MOT.put("END", "AD");
         MOT.put("MOVER", "IS");
-        MOT.put("MOVEM", "IS");
         MOT.put("ADD", "IS");
         MOT.put("DC", "DL");
         MOT.put("DS", "DL");
 
-        int LC = 0; // Location counter
+        int LC = 0;
         Map<String, Integer> symbolTable = new LinkedHashMap<>();
 
-        System.out.println("----- INTERMEDIATE CODE -----");
+        System.out.println("\n----- INTERMEDIATE CODE -----");
         for (String line : program) {
-            String parts[] = line.split("[ ,]+"); // split by space or comma
+            String[] parts = line.split("[ ,]+");
             String opcode = parts[0];
 
-            // START directive
             if (opcode.equals("START")) {
                 LC = Integer.parseInt(parts[1]);
                 System.out.println("(AD,01)\t(C," + LC + ")");
-            }
-            // END directive
+            } 
             else if (opcode.equals("END")) {
                 System.out.println("(AD,02)");
                 break;
             }
-            // Declarative statement DC or DS
             else if (opcode.equals("DC")) {
                 symbolTable.put(parts[0], LC);
                 System.out.println("(DL,01)\t(C," + parts[1] + ")");
@@ -53,22 +49,12 @@ public class PassOneAssembler {
                 System.out.println("(DL,02)\t(C," + parts[1] + ")");
                 LC += Integer.parseInt(parts[1]);
             }
-            // Imperative statements
-            else {
-                // Check if label present
-                int index = 0;
-                if (!MOT.containsKey(parts[0])) {
-                    // It's a label
-                    symbolTable.put(parts[0], LC);
-                    index = 1;
-                    opcode = parts[1];
-                }
-                System.out.println("(" + MOT.get(opcode) + ")\t" + parts[index + 1] + ", " + parts[index + 2]);
+            else { // Imperative statements
+                System.out.println("(" + MOT.get(opcode) + ")\t" + parts[1] + ", " + parts[2]);
                 LC++;
             }
         }
 
-        // Display Symbol Table
         System.out.println("\n----- SYMBOL TABLE -----");
         System.out.println("Symbol\tAddress");
         for (Map.Entry<String, Integer> entry : symbolTable.entrySet()) {
@@ -78,31 +64,21 @@ public class PassOneAssembler {
 }
 
 
-Sample Input
-
+Enter number of lines in program:
+5
+Enter program lines:
 START 100
-MOVER AREG, NUM
+MOVER AREG,NUM 
 ADD BREG, ONE
-MOVEM AREG, RESULT
-ONE DC 1
 NUM DS 1
-RESULT DS 1
 END
 
-
-Sample Output
-
 ----- INTERMEDIATE CODE -----
-(AD,01)	(C,100)
-(IS)	AREG, NUM
-(IS)	BREG, ONE
-(IS)	AREG, RESULT
-(DL,01)	(C,1)
-(DL,02)	(C,1)
-(DL,02)	(C,1)
+(AD,01) (C,100)
+(IS)    AREG, NUM
+(IS)    BREG, ONE
+(null)  DS, 1
 (AD,02)
 
 ----- SYMBOL TABLE -----
-ONE	103
-NUM	104
-RESULT	105
+Symbol  Address
